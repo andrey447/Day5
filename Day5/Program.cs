5eb85fcf3d65db40eacd930fcad650c5dc224664
andrey447 <andrey447@tut.by>
2019-10-26 11:15:40 +0300

    Добавление цвета

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -16,15 +16,44 @@ namespace Laba
             Console.WriteLine("3 - треугольник");
             int num = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Укажите цвет фигуры:");
+            Console.WriteLine("1 - зеленый");
+            Console.WriteLine("2 - красный");
+            Console.WriteLine("3 - синий");
+            int color = Convert.ToInt32(Console.ReadLine());
+
             Figures[] figures = new Figures[3];
             figures[0] = new Rectangle();
             figures[1] = new Rhombus();
             figures[2] = new Triangles();
 
-            foreach (Figures figs in figures)
+            switch (color)
             {
-                figs.Draw();
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
             }
+
+            switch (num)
+            {
+                case 1:
+                    figures[0].Draw();
+                    break;
+                case 2:
+                    figures[1].Draw();
+                    break;
+                case 3:
+                    figures[2].Draw();
+                    break;
+            }
+            
+            Console.ResetColor();
         }
     }
 }
